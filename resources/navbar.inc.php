204e08bfde0cb68b8351d9433869995ba7daf9fc
oliveecherryy <121272155+oliveecherryy@users.noreply.github.com>
2024-12-05 17:09:02 -0500

    fix

--- resources/navbar.inc.php
+++ resources/navbar.inc.php
@@ -3,17 +3,14 @@
             <div class = "dropdown"><a href="../../index.html">olivia lee</a></div>
             <div class = "dropdowndif"><a href="./ec.html" class = "dif_drop">extracurriculars</a></div>
             <div class="dropdowndif">
-              <button class="dropbtn"><a href = "../lab08/projects.html" class = "dif">projects</a>
-                <i class="fa fa-caret-down"></i>
-              </button>
               <div class="dropdown-content">
                 <a href="../lab01/lab01.html">lab 01</a>
                 <a href="../lab02/leeo4-olivialee-resume.html">lab 02</a>
                 <a href="../lab03/iaformat.html">lab 03</a>
                 <a href="../lab04/lab04.html">lab 04</a>
                 <a href="../lab05/lab5.html">lab 05</a>
-                <a href="../lab06/lab6.html">lab 06</a>
-                <a href="#">lab 07</a>
+                <a href="../lab06/lab6.php">lab 06</a>
+                <a href="../lab07/lab7.php">lab 07</a>
                 <a href="../lab08/projects.html">lab 08</a>
                 <a href="../lab09/lab9.html">lab 09</a>
                 <a href="../lab10/lab10.html">lab 10</a>
