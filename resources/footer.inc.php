f6d6fa70b0b654f129dda2627344477b95d0e16b
oliveecherryy <121272155+oliveecherryy@users.noreply.github.com>
2025-01-07 20:14:15 -0500

    html to php

---
+++ resources/footer.inc.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>Olivia Lee<br>
+        LinkedIn: <a href="https://www.linkedin.com/in/olivia-lee-0b00b3269/">Olivia Lee</a><br>
+        School Amail: <a href="mailto:leeo4@rpi.edu">leeo4@rpi.edu</a><br>
+        Personal Email: <a href="mailto:olilee2024@gmail.com">olilee2024@gmail.com</a><br>
+        Feel free to reach out!</p>
+</footer>
